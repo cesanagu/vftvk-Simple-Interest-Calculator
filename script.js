044ca098aab3808bd6e7a861fc881846e733ebd5
Cesar Aguilar <cesar_aguilar.parraga@hotmail.com>
2021-09-03 14:01:53 -0500

    principal variable

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ function updateRate() {
 function compute() {
   var rate = document.getElementById("rate").value;
   var years = document.getElementById("years").value;
-  var p = document.getElementById("principal").value;
-  var interest = (p * years * rate) / 100;
+  var principal = document.getElementById("principal").value;
+  var interest = (principal * years * rate) / 100;
   var year = new Date().getFullYear() + parseInt(years);
   result = document.getElementById("result");
 
-  if (isNaN(p) || p < 1 || p > 1000000) {
+  if (isNaN(principal) || principal < 1 || principal > 1000000) {
     alert("Enter a positive number");
     result.innerHTML = "<br><mark>TRY AGAIN</mark><br>";
   } else {
-    result.innerHTML = `<br>If you deposit <mark>${p}</mark>, <br>at an interest rate of <mark>${rate}%</mark>. 
+    result.innerHTML = `<br>If you deposit <mark>${principal}</mark>, <br>at an interest rate of <mark>${rate}%</mark>. 
     <br>You will receive an amount of <mark>${interest}</mark>,<br>in the year ${year}.<br>`;
   }
 }
